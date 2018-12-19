8cfe37b4dd487a2552af99e45a5e77d62db75fde
Noelle Rolwes <nrolwes@gmail.com>
2018-12-19 07:46:37 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
-
+  post '/' do #CREATE/ reveives form data through params & renders results pg
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    
     erb :results
   end
 end
